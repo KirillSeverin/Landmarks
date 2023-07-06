b82e4309eea6f2459fe616686ab39ed25b8acf17
Kirill Severin <kirillseverin2020@gmail.com>
2023-07-06 23:13:20 +0300

    Created building lists and navigation

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -9,21 +9,30 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(
-            latitude: 34.011_286,
-            longitude: -116.166_868),
-        span: MKCoordinateSpan(
-            latitudeDelta: 2.0,
-            longitudeDelta: 2.0))
+    var coordinate: CLLocationCoordinate2D
+    
+    @State private var region = MKCoordinateRegion()
     
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRigion(coordinate)
+            }
+    }
+    
+    private func setRigion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.2,
+                longitudeDelta: 0.2))
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: CLLocationCoordinate2D(
+            latitude: 4.011_286,
+            longitude: -116.166_868))
     }
 }
